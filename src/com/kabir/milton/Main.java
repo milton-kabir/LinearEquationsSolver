1531527923bed37c70b75331ed14e63afa70991a
milton-kabir <milton.cse.pust@gmail.com>
2021-08-04 21:31:03 +0600

    Second Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -8,8 +8,21 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         Scanner sc = new Scanner(System.in);
-        double a = sc.nextDouble();
-        double b = sc.nextDouble();
-        System.out.println(b / a);
+        String[] a = sc.nextLine().split(" ");
+        String[] b = sc.nextLine().split(" ");
+        double[] ar = new double[a.length];
+        double[] br = new double[b.length];
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = Double.parseDouble(a[i]);
+        }
+        for (int i = 0; i < br.length; i++) {
+            br[i] = Double.parseDouble(b[i]);
+        }
+        double y = br[2] - ar[2] * br[0] / ar[0];
+        y = y / (br[1] - ar[1] * br[0] / ar[0]);
+        double x = ar[2] - ar[1] * y;
+        x /= ar[0];
+        System.out.println(x + " " + y);
+
     }
 }
