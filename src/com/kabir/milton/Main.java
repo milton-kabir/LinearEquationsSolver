8869075902ef661fe017d0312302452e15e4907c
milton-kabir <milton.cse.pust@gmail.com>
2021-08-04 20:13:04 +0600

    First Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -1,8 +1,15 @@
+//package solver;
 package com.kabir.milton;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        System.out.println(b / a);
     }
 }
